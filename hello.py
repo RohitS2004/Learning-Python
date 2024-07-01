642578e78906a029cdb6e26464eca550462a8490
Rohit Saini <rohit.boby2004@gmail.com>
2024-07-01 13:01:22 +0530

    Added some comments

--- hello.py
+++ hello.py
@@ -2,3 +2,5 @@ print('Hello World')
 print(10 / 4) # gives the value with a decimal
 print(10 // 4) # floor division
 print(round(10 / 4)) # rounds off the resultant value
+
+# * NOTE: PYTHON LANGUAGE IS SPACE SENSITIVE MEANS THAT IF YOU PUT ANY UNNECESSARY INDENT THAN THE INTERPRETER WILL RAISE AN ERROR
