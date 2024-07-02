53b43e73fe93fd77119ac888c9e4c8fb8ab90b4e
Rohit Saini <rohit.boby2004@gmail.com>
2024-07-02 11:35:45 +0530

    Added loops file

---
+++ loop.py
@@ -0,0 +1,36 @@
+value = 0
+
+# while (value <= 10): 
+#     print(value)
+#     value += 1
+# else:
+    # else block will occur when the loop has completed but not when the the loop breaks
+    # print("Loop completed")
+
+names = ['Dave', 'John', 'Sara', 'Sasha']
+# for name in names:
+#     print(name)
+
+# for char in "Mississippi":
+#     print(char)
+
+# for num in range(1, 10):
+#     # 1 2 3 4 5 6 7 8 9
+#     print(num)
+
+# for n in range(3):
+#     # 0 1 2
+#     print(n)
+
+# for num in range(0, 11, 2):
+#     print(num)
+
+habbits = ['Eat', 'Code', 'Sleep', 'Repeat']
+
+for name in names:
+    for habbit in habbits:
+        print(f'{name} {habbit}', end=' ')
+    print()
+
+
+
